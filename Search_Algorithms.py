5368a07e8dff0fc033ab707f3ff59d05a138536b
sneka24 <sneka.p2019@vitstudent.ac.in>
2022-09-28 20:34:41 +0530

    Greedy algo

--- Search_Algorithms.py
+++ Search_Algorithms.py
@@ -227,7 +227,7 @@ def astar_search(graph, start, goal,m):#added a new parameter m to pass manhatta
 
             # extra_cost = graph.get_edge_weight(current_node, neighbour)
             extra_cost = 1
-            neighbour_cost = extra_cost + path_cost_till_now + m(neighbour, goal)
+            neighbour_cost = extra_cost + path_cost_till_now + get_geographical_heuristic(neighbour, goal)
             new_element = (neighbour_cost, path_to_neighbour)
 
             is_there, indexx, neighbour_old_cost, _ = get_frontier_params_new(neighbour, frontier)
